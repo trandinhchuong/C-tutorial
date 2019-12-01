de819be9ffd872ea303abee9ccc5d2217eccdae2
root <trandinhchuonght96@gmail.com>
2019-12-01 10:28:54 +0700

    printf

--- input_output/main.c
+++ input_output/main.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+int a;
+int b;
+float c;
 void main( )
 {
-char ch;
-printf("nhap ch= ");
-scanf("%c",&ch); //đọc kí tự ch từ bàn phím
-printf("Ki tu %c la gia tri cua ch",ch);
+
+printf("nhap gia tri a\t");
+scanf("%d",&a); //đọc kí tự ch từ bàn phím
+printf("nhap gia tri b\t");
+scanf("%d",&b); //đọc kí tự ch từ bàn phím
+printf("nhap gia tri c\t");
+scanf("%f",&c); //đọc kí tự ch từ bàn phím
+printf("gia tri cua c=%f \n",c);
 }
---
+++ printf/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a=30;
+int b=40;
+float c=40.555;
+int main()
+{
+    printf("in chu : hello\n");
+    printf("gia tri cua a = %d \n",a);
+    printf("gia tri cua b = %d \n",b);
+    printf("gia tri cua %d+%d= %d \n",a,b,a+b);
+
+    printf("gia tri cua c = %f \n",c);
+}
