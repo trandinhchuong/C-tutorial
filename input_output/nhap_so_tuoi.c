758a40c1aeae57e1b9851895e8be4a3797d56645
chuong <trandinhchuonght96@gmail.com>
2020-02-21 20:19:35 +0700

    var

---
+++ input_output/nhap_so_tuoi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int tuoi=0;
+
+void main( )
+{
+
+printf("nhap so tuoi cua ban \n");
+
+scanf("%d",&tuoi); //đọc kí tự ch từ bàn phím
+printf("tuoi cua ban la %d \n",tuoi);
+
+printf("tuoi cua ban hahi nam sau la  %d \n",tuoi+2);
+
+
+}
