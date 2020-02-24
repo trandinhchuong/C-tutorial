02287a6b7f8adbfc2fff58a315d839a084aea0e2
chuong <trandinhchuonght96@gmail.com>
2020-02-24 19:46:35 +0700

    variables_type

---
+++ variables_types/variables_type.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a=30;
+float b=40.555;
+int main()
+{
+
+    printf("gia tri cua a = %d \n",a);
+    printf("gia tri cua b = %f \n",b);
+
+
+    printf("gia tri cua %d+%f= %f \n",a,b,a+b);
+
+}
