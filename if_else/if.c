ccdffed1aa6e8be7ed266faa8b4a8af2943a5558
chuong <trandinhchuonght96@gmail.com>
2020-02-24 20:50:33 +0700

    if

---
+++ if_else/if.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int a;
+
+void main( )
+{
+
+printf("nhap gia tri a\t\n");
+scanf("%d",&a); //đọc kí tự ch từ bàn phím
+
+if(a>100)
+printf("so vua nhap lon hon 100 \n\n");
+
+
+
+}
